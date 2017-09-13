9d1f39dfea7ed3cba722e7fac282b4de79fc8d75
jeff <jeff@sdu.edu.cn>
2017-09-13 16:47:47 +0800

    cnn kernel_size evaluate

--- dga_classifier/cnn.py
+++ dga_classifier/cnn.py
@@ -12,15 +12,15 @@ from sklearn import feature_extraction
 from sklearn.cross_validation import train_test_split
 
 
-def build_model(max_features):
+def build_model(max_features, kernelSize):
     """Builds logistic regression model"""
     model = Sequential()
 
-    model.add(Conv1D(32, kernel_size=32,
+    model.add(Conv1D(32, kernelSize,
                  activation='relu',
                  input_shape=(max_features, 1)))
 
-    model.add(Conv1D(64, 32, activation='relu'))
+    model.add(Conv1D(64, kernelSize, activation='relu'))
     model.add(MaxPooling1D(pool_size=8))
     model.add(Dropout(0.25))
     model.add(Flatten())
@@ -33,7 +33,8 @@ def build_model(max_features):
     return model
 
 
-def run(max_epoch=20, nfolds=10, batch_size=128):
+
+def run(max_epoch=5, nfolds=10, batch_size=128):
     """Run train/test on logistic regression model"""
     indata = data.get_data()
 
@@ -64,61 +65,63 @@ def run(max_epoch=20, nfolds=10, batch_size=128):
         X_train, X_test, y_train, y_test, _, label_test = train_test_split(count_vec, y,
                                                                            labels, test_size=0.2)
 
+        for kernelSize in (2,4,8,32,64,128):
+            print 'Build model...'
+            model = build_model(max_features, kernelSize)
 
-        print 'Build model...'
-        model = build_model(max_features)
+            print "Train..."
+            X_train, X_holdout, y_train, y_holdout = train_test_split(X_train, y_train, test_size=0.05)
 
-        print "Train..."
-        X_train, X_holdout, y_train, y_holdout = train_test_split(X_train, y_train, test_size=0.05)
+            best_iter = -1
+            best_auc = 0.0
+            out_data = {}
 
-        best_iter = -1
-        best_auc = 0.0
-        out_data = {}
+            for ep in range(max_epoch):
 
-        for ep in range(max_epoch):
+                model.fit(np.expand_dims(X_train.todense(), axis=2), y_train, batch_size=batch_size, nb_epoch=1)
 
-            model.fit(np.expand_dims(X_train.todense(), axis = 2), y_train, batch_size=batch_size, nb_epoch=1)
+                t_probs = model.predict_proba(np.expand_dims(X_holdout.todense(), axis=2))
 
-            t_probs = model.predict_proba(np.expand_dims(X_holdout.todense(), axis = 2))
+                t_auc = sklearn.metrics.roc_auc_score(y_holdout, t_probs)
 
-            t_auc = sklearn.metrics.roc_auc_score(y_holdout, t_probs)
+                print 'Epoch %d: auc = %f (best=%f)' % (ep, t_auc, best_auc)
 
-            print 'Epoch %d: auc = %f (best=%f)' % (ep, t_auc, best_auc)
+                if t_auc > best_auc:
+                    best_auc = t_auc
+                    best_iter = ep
 
-            if t_auc > best_auc:
-                best_auc = t_auc
-                best_iter = ep
+                    probs = model.predict_proba(np.expand_dims(X_test.todense(), axis=2))
+                    pre = model.predict_classes(np.expand_dims(X_test.todense(), axis=2))
 
-                probs = model.predict_proba(np.expand_dims(X_test.todense(), axis = 2))
-                pre = model.predict_classes(np.expand_dims(X_test.todense(), axis = 2))
+                    out_data = {'y': y_test, 'labels': label_test, 'probs': probs, 'epochs': ep,
+                                'confusion_matrix': sklearn.metrics.confusion_matrix(y_test, probs > .5)}
+
+                    accuracy = sklearn.metrics.accuracy_score(y_test, pre)
+                    recall = sklearn.metrics.recall_score(y_test, pre)
+                    f1 = sklearn.metrics.f1_score(y_test, pre)
+
+                else:
+                    # No longer improving...break and calc statistics
+                    if (ep - best_iter) > 5:
+                        break
+
+            final_data.append(out_data)
+
+            with open("f1result.txt", 'a') as f:
+                f.write("##CNN result: \n")
+                f.write("kernelsize: ")
+                f.write(str(kernelSize))
+                f.write(":accuracy: ")
+                f.write(str(accuracy))
+                f.write("; recall: ")
+                f.write(str(recall))
+                f.write("; f1: ")
+                f.write(str(f1))
+                f.write("\n")
 
 
-                out_data = {'y':y_test, 'labels': label_test, 'probs':probs, 'epochs': ep,
-                            'confusion_matrix': sklearn.metrics.confusion_matrix(y_test, probs > .5)}
-
-                accuracy = sklearn.metrics.accuracy_score(y_test, pre)
-                recall = sklearn.metrics.recall_score(y_test, pre)
-                f1 = sklearn.metrics.f1_score(y_test, pre)
-
-            else:
-                # No longer improving...break and calc statistics
-                if (ep-best_iter) > 5:
-                    break
 
 
-
-        final_data.append(out_data)
-
-    with open("f1result.txt", 'a') as f:
-        f.write("##CNN result: \n")
-
-        f.write("accuracy: ")
-        f.write(str(accuracy))
-        f.write("; recall: ")
-        f.write(str(recall))
-        f.write( "; f1: ")
-        f.write(str(f1))
-        f.write("\n")
 
     return final_data
 
